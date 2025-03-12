0d957d2d0878a5c0818a5646aefeea485a1af6b2
Vitor Sergio <VS-7>
2025-03-12 17:02:10 -0300

    Atualizando dados

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,23 +1,23 @@
 export const projects = [
   {
     title: "NutritionTCC",
-    description: "Another interesting project with its own unique description.",
+    description: "O NutritionTCC é um aplicativo desenvolvido em Flutter com Firebase, que utiliza um Algoritmo Genético para otimizar dietas personalizadas. A base de dados conta com 294 alimentos da TACO, garantindo recomendações nutricionais eficientes e adaptadas ao usuário.",
     image: "/assets/projeto3.png",
     slug: "nutritiontcc",
-    repository: "yourusername/another-repo",
+    repository: "VS-7/nutritiontcc",
     url: "https://another-project-url.com"
   },
   {
     title: "Estetify",
-    description: "Detailed description of the project and your role in it.",
+    description: "O Estetify é uma plataforma desenvolvida para o gerenciamento completo de clínicas de estética. Nosso objetivo é otimizar a organização e a eficiência das operações clínicas, proporcionando uma experiência aprimorada para administradores, profissionais e clientes.",
     image: "/assets/projeto1.png",
     slug: "estetify",
-    repository: "yourusername/project-repo",
+    repository: "VS-7/plataforma-estetify",
     url: "https://your-project-url.com"
   },
   {
     title: "Espaço Gardens",
-    description: "Another interesting project with its own unique description.",
+    description: "Esta plataforma foi desenvolvida para o gerenciamento de um espaço de eventos e agendamento de quadras de beach-tennis, proporcionando uma experiência prática e eficiente para os administradores e participantes.",
     image: "/assets/projeto4.png",
     slug: "espaco",
     repository: "yourusername/another-repo",
@@ -25,7 +25,7 @@ export const projects = [
   },
   {
     title: "Crawler Distrituido",
-    description: "Another interesting project with its own unique description.",
+    description: "Este projeto foi desenvolvido para a disciplina de Sistemas Distribuídos, onde foi necessário criar um crawler distribuído para buscar informações de sites de notícias. O projeto foi desenvolvido em Java e utiliza conceitos de programação concorrente e distribuída.",
     image: "/assets/projeto2.png",
     slug: "crawler-distrituidora",
     repository: "yourusername/another-repo",
@@ -33,7 +33,7 @@ export const projects = [
   },
   {
     title: "IFColab - Grupo de Estudos",
-    description: "Another interesting project with its own unique description.",
+    description: "Esta plataforma foi desenvolvida para o gerenciamento de um grupo de estudos, proporcionando uma experiência prática e eficiente para os administradores e participantes.",
     image: "/assets/projeto5.png",
     slug: "ifcolab",
     repository: "yourusername/another-repo",
@@ -41,7 +41,7 @@ export const projects = [
   },
   {
     title: "Paint-Brush",
-    description: "Another interesting project with its own unique description.",
+    description: "Este aplicativo de desenho, inspirado no clássico Paint do Windows, foi desenvolvido em Java utilizando a biblioteca Swing e faz amplo uso de conceitos de orientação a objetos. Com uma interface intuitiva e simples, o usuário pode desenhar livremente, escolher cores, e criar formas geométricas de maneira prática e rápida.",
     image: "/assets/projeto6.png",
     slug: "paint-brush",
     repository: "yourusername/another-repo",
