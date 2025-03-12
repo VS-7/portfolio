6c6ce60e6ba26f78b05ebfb4291cfdb863325110
Vitor Sergio <VS-7>
2025-03-12 15:38:01 -0300

    Corrigindo problemas

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,33 +1,41 @@
 export const projects = [
   {
-    title: "Another Project",
+    title: "NutritionTCC",
     description: "Another interesting project with its own unique description.",
     image: "/assets/projeto3.png",
-    slug: "planetfall",
+    slug: "nutritiontcc",
     repository: "yourusername/another-repo",
     url: "https://another-project-url.com"
   },
   {
-    title: "Project Name",
+    title: "Estetify",
     description: "Detailed description of the project and your role in it.",
     image: "/assets/projeto1.png",
-    slug: "example-project",
+    slug: "estetify",
     repository: "yourusername/project-repo",
     url: "https://your-project-url.com"
   },
   {
-    title: "Another Project",
+    title: "Espaço Gardens",
     description: "Another interesting project with its own unique description.",
-    image: "/assets/projeto2.png",
-    slug: "planetfall",
+    image: "/assets/projeto4.png",
+    slug: "espaco",
     repository: "yourusername/another-repo",
     url: "https://another-project-url.com"
   },
   {
-    title: "Another Project",
+    title: "Crawler Distrituido",
     description: "Another interesting project with its own unique description.",
-    image: "/assets/projeto3.png",
-    slug: "planetfall",
+    image: "/assets/projeto2.png",
+    slug: "crawler-distrituidora",
+    repository: "yourusername/another-repo",
+    url: "https://another-project-url.com"
+  },
+  {
+    title: "IFColab - Grupo de Estudos",
+    description: "Another interesting project with its own unique description.",
+    image: "/assets/projeto5.png",
+    slug: "ifcolab",
     repository: "yourusername/another-repo",
     url: "https://another-project-url.com"
   }
